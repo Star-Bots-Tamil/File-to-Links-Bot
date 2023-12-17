ec2d6b31eb9737826c63ab696ce9f43ad8329ae3
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-12-17 22:47:36 +0530

    Update __init__.py

--- stream/bot/__init__.py
+++ stream/bot/__init__.py
@@ -12,7 +12,6 @@ logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
 from pyrogram import Client
-from database.ia_filterdb import Media
 from info import *
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
